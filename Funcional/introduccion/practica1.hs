0a31893b495cafd03cfe842b0e25209bc854bd27
JoseMarcosH <74742536+JoseMarcosH@users.noreply.github.com>
2021-02-03 14:24:08 -0700

    Practica1
    Practica uno tube problemas con algunas operaciones y no supe hacerlas

---
+++ Funcional/introduccion/practica1.hs
@@ -0,0 +1,19 @@
+suma1 a b = a + b
+suma2 a b c d e = a + b + c + d + e
+resta1 a b = a - b
+resta2 a b c = a - b - c
+multiplicacion1 a b = a * b
+multiplicacion2 a b c = a * b * c 
+division1 a b = a / b
+division2 a b c = a / b / c 
+division3 a b = a `div` b
+division4 a b c = a `div` b `div` c 
+residuo1 a b = a `mod` b
+residuo2 a b c = a `mod` b `mod` c 
+pregunta1 a = not a 
+pregunta2 a = a 
+
+comparacion1 a b = a == b
+comparacion2 a b c = a + b == c 
+comparacion3 a b = a /= b
+comparacion4 a b c = a * b /= c 
