6b6ade366992fe6f30c6f14054ceadc48e093800
JoseMarcosH <74742536+JoseMarcosH@users.noreply.github.com>
2021-02-03 21:33:02 -0700

    Examen unidad 2

---
+++ Funcional/introduccion/examen.hs
@@ -0,0 +1,21 @@
+--1
+promedio a b c d = (a + b + c + d) / 4
+--2
+suma a b c d = a + b + c + d 
+--3
+volumen radio = (4/3)*(pi*radio*radio*radio)
+--4
+mod1 a = mod a 10
+--5
+iguales a b c  = if(a == b)== True
+    then if(b==c )==True
+        then "true"
+        else "false"
+    else "false"
+--6
+dif a b c  = if(a /= b)== False
+    then if(b/=c )==False
+        then "False"
+        else "True"
+    else "True"
+
